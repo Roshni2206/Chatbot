7fd14cd773bc244459b746d16c4c979422853db5
Roshni2206 <135851354+Roshni2206@users.noreply.github.com>
2023-10-30 11:42:44 +0530

    Add files via upload

---
+++ components/Layout/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-light p3">
+        <h4 className="text-center">footer comp</h4>
+        All rights are reserved
+    </div>
+  );
+};
+
+export default Footer
